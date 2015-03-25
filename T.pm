db368133b840548bd10a07427c863120e817b7dd
Tom Browder <tom.browder@gmail.com>
2015-03-25 09:44:04 -0500

    add more tests of idioms

--- T.pm
+++ T.pm
@@ -1,16 +1,25 @@
+
+our %attrs = (a=>1,b=>2,c=>3);
+
 class T;
 
 has $.a is rw;
 has $.b is rw;
 has $.c is rw;
+has $.d is rw;
 
 submethod BUILD(
 		# set defaults here
 		:$!a = 15,
 		:$!b,
 		:$!c,
+                :$!d = 100,
 	       ) {
   self.set_b;
+}
+
+method d($arg) {
+  say "\$arg is '$arg'";
 }
 
 multi method set_b {
@@ -24,4 +33,11 @@ multi method set_b {
 
 multi method set_b($x) {
   self.b = $x;
+}
+
+method show {
+  for %attrs.kv -> $k, $v {
+    my $aval = self."$k"();  # supposed to work for a method name
+    say "DEBUG:  For attr '$k', value is '$aval'";
+  }
 }
